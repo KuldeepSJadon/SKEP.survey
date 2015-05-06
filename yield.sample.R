44aea9cb5016c04a751ec391ce6c62c82936e43d
sithjaisong <sith.jaisong@gmail.com>
2015-05-06 10:10:07 +0800

    delete

--- yield.sample.R
+++
@@ -1,4 +0,0 @@
-sum.sheet4 <- data.sheet4 %.% 
-        mutate(m.Y = (Y/MC)*14, yield.kg.ha = m.Y*2000) %>%
-        group_by(season, year, treatment, rep) %>% 
-        summarise( mean.ykh = mean(yield.kg.ha))
